6c3e106d623e73ee8a87b30f4b76fca15595bbfe
onurozer0 <onurozer1881@gmail.com>
2024-10-05 15:32:43 +0300

    add Entities
    https://volkankarakulakk-1727687394263.atlassian.net/browse/P2-3

---
+++ HotelBook.Data/Entities/RoomType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelBook.Data.Entities
+{
+	public enum RoomType : byte
+	{
+		Classic = 1,
+		Vip,
+		VipPlus
+	}
+}
